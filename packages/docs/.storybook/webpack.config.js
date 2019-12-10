e43fc8013a04cf979ca70d317f2aec5822792b64
Erik Hughes <erik.hughes@outlook.com>
2019-12-10 14:23:39 +0800

    build: changed typescript process reducing build by 15s

--- packages/docs/.storybook/webpack.config.js
+++ packages/docs/.storybook/webpack.config.js
@@ -1,7 +1,23 @@
 module.exports = ({ config }) => {
-  // Remove default postcss options allowing configs to be read from individual packages
+  // Remove default PostCSS options allowing configs to be read from individual packages
   const cssRule = config.module.rules.find(r => r.test.toString() === /\.css$/.toString());
   const postCssUse = cssRule.use.find(u => u.loader && u.loader.indexOf('postcss-loader') > -1);
   delete postCssUse.options;
+
+  // Add TypeScript handling to Babel configuration
+  const jsRule = config.module.rules.find(r => r.test.toString() === /\.(mjs|jsx?)$/.toString());
+  const babelUse = jsRule.use.find(u => u.loader && u.loader.indexOf('babel-loader') > -1);
+  jsRule.test = /\.(mjs|jsx?|tsx?)$/;
+  jsRule.exclude = /node_modules\/(?!@trutoo)/;
+  delete jsRule.include;
+  babelUse.options.presets.push([
+    require.resolve('@babel/preset-typescript'),
+    {
+      isTSX: true,
+      allExtensions: true,
+    },
+  ]);
+  config.resolve.extensions.push('.ts', '.tsx');
+
   return config;
 };
