86c123ab7ab3d9a77848d35bbebdd830218d811b
Erik Hughes <erik.hughes@outlook.com>
2019-12-16 10:16:21 +0100

    fix: docs deploy now requires an actor

--- packages/docs/.storybook/deploy.js
+++ packages/docs/.storybook/deploy.js
@@ -7,11 +7,16 @@ const metadata = require('../package.json');
 
 const main = async () => {
   try {
+    if (!process.env.GITHUB_ACTOR)
+      throw new Error('You must specify an actor whom performs the commit using GITHUB_ACTOR env variable');
+    if (!process.env.GITHUB_TOKEN)
+      throw new Error('You must specify a token authenticating the commit using GITHUB_TOKEN env variable');
+
     console.log('Determining origin url');
     const origin = await git.getRemoteUrl('origin');
 
     const { host, repo } = parse(origin);
-    const url = `https://${process.env.GITHUB_TOKEN}@${host}/${repo}`;
+    const url = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@${host}/${repo}`;
 
     console.log(`Publishing documentation to ${url} gh-pages branch`);
     await new Promise((resolve, reject) => {
