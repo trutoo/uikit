3d65b63de721f9a7bb5d74e0bf6acd945ba3ab44
Erik Hughes <erik.hughes@outlook.com>
2020-01-08 14:14:16 +0100

    fix: removed center plugin and imporved general presentation

--- packages/docs/.storybook/config.jsx
+++ packages/docs/.storybook/config.jsx
@@ -3,7 +3,6 @@ import { configure, addDecorator, addParameters } from '@storybook/react';
 import addon from '@storybook/addons';
 import { withA11y } from '@storybook/addon-a11y';
 import { themes } from '@storybook/theming';
-import centered from '@storybook/addon-centered/react';
 
 import icons from '!raw-loader!@trutoo/ui-icons/dist/symbols.svg';
 
@@ -27,12 +26,14 @@ addParameters({
 
 addDecorator((storyFn, context) => {
   return (
-    <div className="e-container e-elevation-1" style={{ marginTop: '2rem', paddingTop: '1rem', minHeight: '50vh' }}>
-      <div style={{ display: 'none' }} dangerouslySetInnerHTML={{ __html: icons }}></div>
-      <h1 style={{ fontSize: '1.5em' }}>{context.kind}</h1>
-      <strong style={{ color: 'var(--c_text_low)' }}>{context.name}</strong>
-      <hr />
-      {storyFn()}
+    <div className="e-container" style={{ paddingTop: '2rem', paddingBottom: '2rem' }}>
+      <div className="e-elevation-1" style={{ padding: '1rem' }}>
+        <div style={{ display: 'none' }} dangerouslySetInnerHTML={{ __html: icons }}></div>
+        <h1 style={{ fontSize: '1.5em' }}>{context.kind}</h1>
+        <strong style={{ color: 'var(--c_text_low)' }}>{context.name}</strong>
+        <hr />
+        {storyFn()}
+      </div>
     </div>
   );
 });
