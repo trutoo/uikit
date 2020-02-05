9aa02b6a987eb192664dcebca9870df30e768205
Erik Hughes <erik.hughes@outlook.com>
2020-02-05 12:09:11 +0100

    ci: improved github pages release

--- packages/docs/.storybook/deploy.js
+++ packages/docs/.storybook/deploy.js
@@ -1,48 +1,45 @@
 const ghpages = require('gh-pages');
 const Git = require('gh-pages/lib/git');
-const parse = require('parse-github-url');
+const argv = require('minimist')(process.argv.slice(2));
+const gitParse = require('git-url-parse');
 
 const git = new Git(process.cwd(), 'git');
 const metadata = require('../package.json');
 
 const main = async () => {
-  try {
-    if (!process.env.GITHUB_ACTOR)
-      throw new Error('You must specify an actor whom performs the commit using GITHUB_ACTOR env variable');
-    if (!process.env.GITHUB_TOKEN)
-      throw new Error('You must specify a token authenticating the commit using GITHUB_TOKEN env variable');
+  if (!process.env.GITHUB_ACTOR)
+    throw new Error('You must specify an actor whom performs the commit using GITHUB_ACTOR env variable');
+  if (!process.env.GITHUB_TOKEN)
+    throw new Error('You must specify a token authenticating the commit using GITHUB_TOKEN env variable');
 
-    console.log('Determining origin url');
-    const origin = await git.getRemoteUrl('origin');
+  console.log('Determining origin url');
+  const origin = await git.getRemoteUrl('origin');
 
-    const { host, repo } = parse(origin);
-    const url = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@${host}/${repo}`;
+  const { resource, full_name } = gitParse(origin);
+  const url = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@${resource}/${full_name}.git`;
 
-    console.log(`Publishing documentation to ${url} gh-pages branch`);
-    await new Promise((resolve, reject) => {
-      ghpages.publish(
-        'dist',
-        {
-          repo: url,
-          message: `chore(release): documentation v${metadata.version}`,
-        },
-        err => {
-          if (err) reject(err);
-          resolve();
-        },
-      );
-    });
-  } catch (err) {
-    console.error(err);
-    throw err;
-  }
+  console.log(`Publishing documentation to ${url} gh-pages branch`);
+  await new Promise((resolve, reject) => {
+    ghpages.publish(
+      'dist',
+      {
+        repo: url,
+        message: `chore(release): documentation v${metadata.version}`,
+      },
+      err => {
+        if (err) reject(err);
+        resolve();
+      },
+    );
+  });
 };
 
 main()
   .then(() => {
-    console.log(`Sucessfully published documentation to GitHub Pages`);
+    console.log(`\n\x1b[36mSucessfully published documentation to GitHub Pages\x1b[0m\n`);
   })
-  .catch(() => {
-    console.error(`Failed to publish documentation to GitHub Pages`);
+  .catch(err => {
+    if (argv.v || argv.verbose) console.error(err);
+    console.error(`\n\x1b[31mFailed to publish documentation to GitHub Pages\x1b[0m\n`);
     process.exit(1);
   });
