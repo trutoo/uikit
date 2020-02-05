570bb04df37bbc2d96f37d070292bda17bf2c386
Erik Hughes <erik.hughes@outlook.com>
2020-02-05 12:08:36 +0100

    docs: updated commit and changelog tools

--- scripts/commit-msg.js
+++ scripts/commit-msg.js
@@ -1,23 +1,24 @@
 'use strict';
 const commit = require('fs').readFileSync(process.env.HUSKY_GIT_PARAMS, 'utf8');
 
-if (/^(merge|chore\(release\))/i.test(commit)) {
+if (/^(merge|chore\((release|deps)\): .+)/i.test(commit)) {
   process.exit(0);
 }
 
-if (!/^(feat|fix|perf|revert|docs|style|refactor|test|build|ci)(?:\(.+\))?: /.test(commit)) {
+if (!/^(feat|fix|perf|k8s|revert|docs|style|refactor|test|build|ci|)(\(.+?\))?: .+/.test(commit)) {
   console.warn(
-    `Git commit message should lead with a type, followed by an optional scope in parentheses, then a colon, a space, and a message. "feat(typeahead): #1337 added keyboard interaction".`,
+    'Git commit message should lead with a type, followed by an optional scope in parentheses, then a colon, a space, and a message.',
   );
 
-  console.warn(`Valid types are feat|fix|perf|revert|docs|style|refactor|test|build|ci.`);
+  console.warn(
+    'Valid types are feat|fix|perf|k8s|revert|docs|style|refactor|test|build|ci and special merge|chore(deps)|chore(release).',
+  );
 
-  if (/^(feat|fix)(?:\(.+?\))?: /.test(commit) && !/#\d+/.test(commit)) {
-    console.warn(`Features and fixes changes should include a task id.`);
-    console.warn(`Example: "feat(typeahead): #1337 added keyboard interaction".`);
-  } else {
-    console.warn(`Example: "ci(changelog): build step added for conventional changelogs".`);
-  }
+  console.warn('Features, fixes and performance changes should include a task id if applicable.');
+  console.warn('Example: "feat(typeahead): #1337 added keyboard interaction".');
 
-  process.exit(1337);
+  console.warn('All other commits follow the standard semantics.');
+  console.warn('Example: "ci(changelog): build step added for conventional changelogs".');
+
+  process.exit(1);
 }
