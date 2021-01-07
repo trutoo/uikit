60b18851fa6981d2f3bcf002cc328c0705ae3b6e
Erik Hughes <erik.hughes@outlook.com>
2021-01-07 21:56:03 +0100

    feat: updated dependencies and configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,43 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
-    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'eslint:recommended', // Default base
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended', // Disables rules that conflict with Prettier.
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:prettier/recommended', // Disables rules that conflict with Prettier
+    'prettier/@typescript-eslint', // Disables @typescript-eslint rules that conflict with Prettier
+    'prettier/react', // Disables react rules that conflict with Prettier
   ],
-  plugins: ['react', '@typescript-eslint', 'prettier'], // Extends ESLint with rules and reports differences as individual ESLint issues
   parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    ecmaVersion: 2021, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
-    ecmaFeatures: {
-      jsx: true, // Allows for the parsing of JSX
-    },
   },
   rules: {
+    /* Important */
+    'prettier/prettier': [
+      'warn',
+      {
+        endOfLine: 'auto',
+      },
+    ],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'prettier/prettier': 'warn',
+    /* General */
+    'lines-between-class-members': ['warn', 'always', { exceptAfterSingleLine: true }],
+    /* TypeScript */
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: false,
+        argsIgnorePattern: '^_',
+      },
+    ],
   },
   overrides: [
     {
